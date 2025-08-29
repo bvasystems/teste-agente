b741f811b89e56e7d7b07b6f80609253745ef81f
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-08-29 11:12:03 -0300

    feat: add FileParser and update static knowledge file
    Update the agent configuration to use FileParser for document parsing and change the static knowledge reference from arthur.md to curriculum.pdf

--- examples/static_knowledge.py
+++ examples/static_knowledge.py
@@ -2,11 +2,13 @@ from agentle.agents.agent import Agent
 from agentle.generations.providers.google.google_generation_provider import (
     GoogleGenerationProvider,
 )
+from agentle.parsing.parsers.file_parser import FileParser 
 
 
 agent = Agent(
     generation_provider=GoogleGenerationProvider(),
-    static_knowledge=["examples/arthur.md"],
+    static_knowledge=["examples/curriculum.pdf"],
+    document_parser=FileParser(),
     instructions="""Você é uma assistente de IA responsável por responder perguntas e conversar, de maneira educada, sobre o Arthur.""",
 )
 
